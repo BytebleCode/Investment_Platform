2ace4dcdaa8db7cad9be73aaa3a82ab258872141
Brock Varzaly <ascendtechnology22@gmail.com>
2026-02-19 11:49:12 -0500

    Allow CDN resources in Content Security Policy for production

--- investment_platform/app/security.py
+++ investment_platform/app/security.py
@@ -115,10 +115,10 @@ def configure_security_headers(app: Flask):
         if os.getenv('FLASK_ENV') == 'production':
             response.headers['Content-Security-Policy'] = (
                 "default-src 'self'; "
-                "script-src 'self' 'unsafe-inline' 'unsafe-eval'; "
-                "style-src 'self' 'unsafe-inline'; "
+                "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://cdn.jsdelivr.net; "
+                "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
                 "img-src 'self' data: https:; "
-                "font-src 'self' data:; "
+                "font-src 'self' data: https://fonts.gstatic.com; "
                 "connect-src *;"
             )
 
